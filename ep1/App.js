c7ae01d45536096b663cc6eb0349833424fea1d9
Soumyajit <4soumya19@gmail.com>
2024-06-13 19:42:33 +0530

    ep2

---
+++ ep1/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () =>{
+    return(
+        <div className="header">
+
+        
+            <div>
+                <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?size=2 "  />
+
+            </div>
+      </div>
+
+
+    )
+}
+
+const AppLayout = () =>{
+    return <div class="app"></div>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render()
